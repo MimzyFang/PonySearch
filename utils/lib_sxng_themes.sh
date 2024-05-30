9248b7013770d9c794a8d0d99978da47520f02b8
Sky Splash <the@birb.horse>
2024-05-30 13:45:02 +0200

    Revert "attempt to add the marlo theme to the theme building process"
    This reverts commit f54d7c74a2d16179ca9706932f6aaf5b88edccc7.

--- utils/lib_sxng_themes.sh
+++ utils/lib_sxng_themes.sh
@@ -12,9 +12,6 @@ themes.:
   simple.:
     build   : build simple theme
     test    : test simple theme
-  marlo.:
-    build   : build simple theme
-    test    : test simple theme
 EOF
 }
 
@@ -23,7 +20,6 @@ themes.all() {
         pygments.less
         node.env
         themes.simple
-        themes.marlo
     )
     dump_return $?
 }
@@ -60,26 +56,10 @@ themes.simple() {
     dump_return $?
 }
 
-themes.marlo() {
-    (   set -e
-        build_msg GRUNT "theme: marlo"
-        npm --prefix searx/static/themes/marlo run build
-    )
-    dump_return $?
-}
-
 themes.simple.test() {
     build_msg TEST "theme: simple"
     nodejs.ensure
     npm --prefix searx/static/themes/simple install
     npm --prefix searx/static/themes/simple run test
-    dump_return $?
-}
-
-themes.marlo.test() {
-    build_msg TEST "theme: marlo"
-    nodejs.ensure
-    npm --prefix searx/static/themes/marlo install
-    npm --prefix searx/static/themes/marlo run test
     dump_return $?
 }
